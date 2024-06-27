2a24ad609381c839812e4596d2d3e6d8a1c025cd
mrinmoy <priyadarsanf2@gmail.com>
2024-06-27 17:46:48 +0530

    This is the files for rest api

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const fs = require("fs")
+const users = require("./MOCK_DATA.json")
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false })) // this middileware is use for: post req data -> objet
+
+//Routes :
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html)
+})
+
+// REST API
+
+app.get('/api/users', (req, res) => {
+    return res.json(users)
+})
+
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+}).patch((req, res) => {
+    // TODO : Create new user
+    return res.json({ status : "pending.." })
+}).delete((req, res) => {
+     // TODO : delete the user with id
+     return res.json({ status: "pending..." })
+})
+
+app.post('/api/users', (req , res) => {
+    // TODO : Create new user
+    const body = req.body
+    users.push({...body, id: users.length + 1 })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status : "success...", id : users.length })
+    })
+})
+
+
+app.listen(PORT , () => {
+    console.log(`Server Started at Port ${PORT}`)
+})
